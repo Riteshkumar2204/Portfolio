2ff3392a67a3bbba55e1347e80dd4f8fafc09b84
Riteshkumar2204 <rk8958496098@gmail.com>
2025-07-25 20:02:51 +0530

    resume

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
       .then(
         () => {
           setIsSent(true);
-          form.current.reset(); // Reset form fields after sending
+          form.current.reset(); 
           toast.success("Message sent successfully! ✅", {
             position: "top-right",
             autoClose: 3000,
@@ -51,10 +51,10 @@ const Contact = () => {
       id="contact"
       className="flex flex-col items-center justify-center py-24 px-[12vw] md:px-[7vw] lg:px-[20vw]"
     >
-      {/* Toast Container */}
+      
       <ToastContainer />
 
-      {/* Section Title */}
+     
       <div className="text-center mb-16">
         <h2 className="text-4xl font-bold text-white">CONTACT</h2>
         <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
@@ -63,7 +63,7 @@ const Contact = () => {
         </p>
       </div>
 
-      {/* Contact Form */}
+      
       <div className="mt-8 w-full max-w-md bg-[#0d081f] p-6 rounded-lg shadow-lg border border-gray-700">
         <h3 className="text-xl font-semibold text-white text-center">
           Connect With Me <span className="ml-1">🚀</span>
@@ -99,7 +99,7 @@ const Contact = () => {
             className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500"
           />
           
-          {/* Send Button */}
+          
           <button
             type="submit"
             className="w-full bg-gradient-to-r from-purple-600 to-pink-500 py-3 text-white font-semibold rounded-md hover:opacity-90 transition"
--- portfolio/src/components/Education/Education.jsx
+++ portfolio/src/components/Education/Education.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { education } from "../../constants"; // Import the education data
+import { education } from "../../constants"; 
 
 const Education = () => {
   return (
@@ -7,7 +7,7 @@ const Education = () => {
       id="education"
       className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom-3"
     >
-      {/* Section Title */}
+      
       <div className="text-center mb-16">
         <h2 className="text-4xl font-bold text-white">EDUCATION</h2>
         <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
@@ -16,12 +16,12 @@ const Education = () => {
         </p>
       </div>
 
-      {/* Education Timeline */}
+    
       <div className="relative">
-        {/* Vertical line */}
+        
         <div className="absolute sm:left-1/2 left-0 transform -translate-x-1/2 sm:-translate-x-0 w-1 bg-white h-full"></div>
 
-        {/* Education Entries */}
+        
         {education.map((edu, index) => (
           <div
             key={edu.id}
@@ -38,15 +38,15 @@ const Education = () => {
               />
             </div> */}
 
-            {/* Content Section */}
+            
             <div
               className={`w-full sm:max-w-md p-4 sm:p-8 rounded-2xl shadow-2xl border border-white bg-gray-900 backdrop-blur-md shadow-[0_0_20px_1px_rgba(130,69,236,0.3)] ${
                 index % 2 === 0 ? "sm:ml-0" : "sm:mr-0"
               } sm:ml-44 sm:mr-44 ml-8 transform transition-transform duration-300 hover:scale-105`}
             >
-              {/* Flex container for image and text */}
+              
               <div className="flex items-center space-x-6">
-                {/* School Logo/Image */}
+                
                 <div className="w-24 h-16 bg-white rounded-md overflow-hidden">
                   <img
                     src={edu.img}
@@ -55,7 +55,7 @@ const Education = () => {
                   />
                 </div>
 
-                {/* Degree, School Name, and Date */}
+                
                 <div className="flex flex-col justify-between">
                   <div>
                     <h3 className="text-xl sm:text-xl font-semibold text-white">
@@ -65,7 +65,7 @@ const Education = () => {
                       {edu.school}
                     </h4>
                   </div>
-                  {/* Date at the bottom */}
+                  
                   <p className="text-sm text-gray-500 mt-2">{edu.date}</p>
                 </div>
               </div>
