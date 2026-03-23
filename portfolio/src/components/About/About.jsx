99bfb0336559577c93115d9c68f9f9d5f8283613
Riteshkumar2204 <rk8958496098@gmail.com>
2026-03-24 00:03:02 +0530

    resume change

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           
           <a
-            href="https://drive.google.com/file/d/1WTIqGKnscbDORHTUn4DyoCso1NB7wkMd/view?usp=sharing "
+            href="https://drive.google.com/file/d/1L26cRvYvZVZ7f2fE2vPnoOKzLmKGWG8N/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
